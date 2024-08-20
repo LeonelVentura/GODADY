604abf106112da9836d50f380a055a214a9673df
GabrielOmar13 <134768512+GabrielOmar13@users.noreply.github.com>
2024-08-19 21:52:36 -0500

    Delete Calendario directory

--- Calendario/servidor/eventos/editar1.php
+++
@@ -1,6 +0,0 @@
-<?php //session_start();
-    include "../../clases/Eventos1.php";
-    $Eventos = new Eventos();
-    $id_evento = $_POST['id_evento'];
-    echo json_encode($Eventos->editarEvento($id_evento));
-?>
--- Calendario/servidor/eventos/eliminar1.php
+++
@@ -1,6 +0,0 @@
-<?php //session_start();
-    include "../../clases/Eventos1.php";
-    $Eventos = new Eventos();
-    $id_evento = $_POST['id_evento'];
-    echo $Eventos->eliminarEvento($id_evento);
-?>
